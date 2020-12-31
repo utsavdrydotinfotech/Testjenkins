711ec664eb4b922f045d1d53b30309d997d1adb0
root <root@test.test.test>
2020-12-31 17:26:59 +0530

    c

--- Hello.java
+++ Hello.java
@@ -5,6 +5,6 @@ public class Hello
 {
     public static void main (String args[]) 
     {
-         System.out.println ("Hello check command's outuput in jenkins server !");
+         System.out.println ("Hello ud !");
     }
 }
