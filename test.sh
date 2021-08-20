371ba0a4f3f61c5c56490f94633e6e2b56d9281d
root <utsavdrydotinfotech.com>
2021-08-20 12:10:24 +0000

    testing

--- test.sh
+++ test.sh
@@ -1,2 +1,12 @@
-testing
-ca
+echo "Start!"
+for p in test.txt
+do
+	if [ $p=dist ]
+    	    then
+			echo "eveything is good" > good.txt
+                	ls
+	else
+			echo "ok did't do anything " > free.txt
+	fi
+done
+
