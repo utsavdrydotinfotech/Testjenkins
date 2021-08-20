dc00fdacc16050215eb36564a5ea4cffba45a8ac
root <utsavdrydotinfotech.com>
2021-08-20 12:04:29 +0000

    testing

--- test.sh
+++ test.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-while read oldrev newrev refname
-do
-    # list of changed files for a commit
-    filelist=$(git diff-tree --no-commit-id --name-only -r $newrev)
-    if [[ $filelist == *"dist"* ]]; then
-       http://65.2.63.174:8080/job/test/build?token=utsavtest
-    fi
-done
-
+testing
